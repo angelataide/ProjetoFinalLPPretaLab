c39e8718963fcd3413735bd9b4b1f07cc562795f
Angela <angel.santosdev@gmail.com>
2024-12-12 20:11:29 -0300

    Descrição clara das atualizações

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,12 +1,11 @@
 function somar() {
   const numero1 = parseFloat(document.getElementById("numero1Soma").value);
   const numero2 = parseFloat(document.getElementById("numero2Soma").value);
-  // evite mexer no código acima!
+  
 
-  const resultado = 0;
+  const resultado = numero1 + numero2;
 
-  /* use as constantes numero 1 e numero 2 e em seguida armazene na variavel a operação descrita no nome da função.
-  Em seguida, ao invés de mostrar o numero 0, mostre o resultado da operação */
+  /* Mostra o resultado da operação */
   document.getElementById("resultadoSoma").innerHTML = resultado;
 }
 
@@ -15,33 +14,32 @@ function subtrair() {
   const numero2 = parseFloat(document.getElementById("numero2Sub").value);
   // evite mexer no código acima!
 
-  const resultado = 0;
+  const resultado = numero1 - numero2;
 
-  /* use as constantes numero 1 e numero 2 e em seguida armazene na variavel a operação descrita no nome da função.
-   Em seguida, ao invés de mostrar o numero 0, mostre o resultado da operação */
+  /* Mostra o resultado da operação */
   document.getElementById("resultadoSub").innerHTML = resultado;
 }
 
 function multiplicar() {
   const numero1 = parseFloat(document.getElementById("numero1Mult").value);
   const numero2 = parseFloat(document.getElementById("numero2Mult").value);
-  // evite mexer no código acima!
+  
 
-  const resultado = 0;
+  const resultado = numero1 * numero2;
 
-  /* use as constantes numero 1 e numero 2 e em seguida armazene na variavel a operação descrita no nome da função.
-   Em seguida, ao invés de mostrar o numero 0, mostre o resultado da operação */
+  /* Mostra o resultado da operação */
   document.getElementById("resultadoMult").innerHTML = resultado;
 }
 
 function dividir() {
   const numero1 = parseFloat(document.getElementById("numero1Div").value);
   const numero2 = parseFloat(document.getElementById("numero2Div").value);
-  // evite mexer no código acima!
+  
 
-  const resultado = 0;
-
-  /* use as constantes numero 1 e numero 2 e em seguida armazene na variavel a operação descrita no nome da função.
-   Em seguida, ao invés de mostrar o numero 0, mostre o resultado da operação */
-  document.getElementById("resultadoDiv").innerHTML = resultado;
+  if (numero2 !== 0) {
+    const resultado = numero1 / numero2;
+    document.getElementById("resultadoDiv").innerHTML = resultado;
+  } else {
+    document.getElementById("resultadoDiv").innerHTML = "Erro: Divisão por zero!";
+  }
 }
